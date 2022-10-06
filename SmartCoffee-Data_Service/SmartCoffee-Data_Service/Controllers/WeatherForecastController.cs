accadc19d7c7616a882ae2a5a9aefe40fdcdf856
Rense <rensevanstiphout@gmail.com>
2022-10-06 10:32:15 +0200

    test2

--- SmartCoffee-Data_Service/SmartCoffee-Data_Service/Controllers/WeatherForecastController.cs
+++ SmartCoffee-Data_Service/SmartCoffee-Data_Service/Controllers/WeatherForecastController.cs
@@ -31,5 +31,18 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get2()
+    {
+        string username = "Admin";
+        string password = "Admin";
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
 
